0605c320b1deb30ffca71370799e7d09a6b29720
jiwoncpark <jiwon.christine.park@gmail.com>
2019-10-16 23:48:57 -0700

    Fix syntax errors in download script (bug)

--- h0rton/tdlmc_data/download_tdlmc_data.sh
+++ h0rton/tdlmc_data/download_tdlmc_data.sh
@@ -1,11 +1,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Rung 0
 wget https://tdlmc.github.io/data/rung0.tar.gz -P $DIR
 wget https://tdlmc.github.io/data/rung0_open_box.tar.gz -P $DIR
+tar -xzvf $DIR/rung0.tar.gz -C $DIR
+tar -xzvf $DIR/rung0_open_box.tar.gz -C $DIR
+# Rung 1
+wget https://tdlmc.github.io/data/rung1.tar.gz -P $DIR
+wget https://tdlmc.github.io/data/rung1_open_box.zip -P $DIR
 wget https://tdlmc.github.io/data/oversampled_PSF.fits -P $DIR
+tar -xzvf $DIR/rung1.tar.gz -C $DIR
+unzip $DIR/rung1_open_box.zip -d $DIR
+# Rung 2
 wget https://tdlmc.github.io/data/rung2.tar.gz -P $DIR
 wget https://tdlmc.github.io/data/rung2_open_box.zip -P $DIR
+tar -xzvf $DIR/rung2.tar.gz -C $DIR
+unzip $DIR/rung2_open_box.zip -d $DIR
+# Rung 3
 wget https://tdlmc.github.io/data/rung3.tar.gz -P $DIR
 wget https://tdlmc.github.io/data/rung3_open_box.zip -P $DIR
-tar -xzvf rung0.tar.gz rung0_open_box.tar.gz rung2.tar.gz rung3.tar.gz -C $DIR
-unzip rung2_open_box.zip rung3_open_box.zip -d $DIR
-tar -xzvf $DIR/rung0.tar.gz -C $DIR
+tar -xzvf $DIR/rung3.tar.gz -C $DIR
+unzip $DIR/rung3_open_box.zip -d $DIR
