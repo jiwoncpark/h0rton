4ca7fd1debe47a7c0be1f48da162ffa3ca3544e4
jiwoncpark <jiwon.christine.park@gmail.com>
2019-10-24 11:25:02 -0700

    Rename log gaussian to gaussian ll to avoid confusion

--- h0rton/tests/test_h0_inference/test_h0_posterior.py
+++ h0rton/tests/test_h0_inference/test_h0_posterior.py
@@ -5,19 +5,20 @@ import unittest
 class TestH0Posterior(unittest.TestCase):
     """A suite of tests verifying that the input PDFs and the sample distributions
     match.
+    
     """
 
-    def test_generalized_normal(self):
+    def test_gaussian_ll_pdf(self):
         """Test the log normal PDF
 
         """
-        from h0rton.h0_inference import log_normal_pdf
+        from h0rton.h0_inference import gaussian_ll_pdf
         from scipy.stats import norm
         m = 0.5
         s = 1
         eval_x = np.linspace(-3, 3, 100)
         truth = np.log(norm.pdf(eval_x, m, s))
-        pred = log_normal_pdf(eval_x, m, s)
+        pred = gaussian_ll_pdf(eval_x, m, s)
         np.testing.assert_array_almost_equal(pred, truth)
 
 if __name__ == '__main__':
