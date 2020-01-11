0e5159364809b5c3f62929ddb55dc08c0c381f02
jiwoncpark <jiwon.christine.park@gmail.com>
2020-01-10 18:54:11 -0800

    Have H0 histogram plotter return the mean and std fit values so that they can be used in the TDLMC metric evaluator

--- h0rton/h0_inference/plotting_utils.py
+++ h0rton/h0_inference/plotting_utils.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -7,7 +8,7 @@ __all__ = ['plot_h0_histogram']
 def gaussian(x, mean, amplitude, standard_deviation):
     return amplitude * np.exp( - ((x - mean) / standard_deviation) ** 2)
 
-def plot_h0_histogram(h0_dict, lens_i, true_h0):
+def plot_h0_histogram(h0_dict, lens_i, true_h0, save_dir='.'):
     """Plot the histogram of H0 samples, overlaid with a Gaussian fit and truth H0
 
     """
@@ -16,10 +17,14 @@ def plot_h0_histogram(h0_dict, lens_i, true_h0):
     bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
     popt, _ = curve_fit(gaussian, bin_centers, bin_heights, p0=[75.0, 0.3, 3.0])
     x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
-    plt.plot(x_interval_for_fit, gaussian(x_interval_for_fit, *popt), color='k', label='fit: mu={:0.1f}, sig={:0.1f}'.format(popt[0], popt[-1]))
+    mean = popt[0]
+    std = popt[-1]
+    plt.plot(x_interval_for_fit, gaussian(x_interval_for_fit, *popt), color='k', label='fit: mu={:0.1f}, sig={:0.1f}'.format(mean, std))
     plt.xlabel('H0 (km/Mpc/s)')
     plt.ylabel('density')
     plt.title('h0_histogram_{0:04d}'.format(lens_i))
     plt.legend()
-    plt.savefig('h0_histogram_{0:04d}.png'.format(lens_i))
+    save_path = os.path.join(save_dir, 'h0_histogram_{0:04d}.png'.format(lens_i))
+    plt.savefig(save_path)
     plt.close()
+    return mean, std
