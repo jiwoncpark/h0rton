ff326f512dd0f8b0f93ba734aa457b2962f04f1d
jiwoncpark <jiwon.christine.park@gmail.com>
2020-01-30 15:23:43 -0800

    Enable optional saving of each H0 histogram with Gaussian fit

--- h0rton/h0_inference/plotting_utils.py
+++ h0rton/h0_inference/plotting_utils.py
@@ -12,18 +12,25 @@ def plot_h0_histogram(samples, weights, lens_i=0, true_h0=None, save_dir='.'):
     """Plot the histogram of H0 samples, overlaid with a Gaussian fit and truth H0
 
     """
-    bin_heights, bin_borders, _ = plt.hist(samples, weights=weights, bins=60, alpha=0.5, density=True, edgecolor='k', color='tab:blue', range=[40.0, 100.0])
+    bin_heights, bin_borders, _ = plt.hist(samples, weights=weights, bins=40, alpha=0.5, density=True, edgecolor='k', color='tab:blue', range=[50.0, 100.0])
     bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
     best_guess_mean = bin_centers[np.argmax(bin_heights)]
-    #print(best_guess_mean)
     popt, _ = curve_fit(gaussian, bin_centers, bin_heights, p0=[best_guess_mean, 0.3, 3.0], maxfev=10000)
-    x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
     mean = popt[0]
     std = popt[-1]
+    #if std < 1.0:
+    #    bin_heights, bin_borders, _ = plt.hist(samples, weights=weights, bins=80, alpha=0.5, density=True, edgecolor='k', color='tab:blue', range=[mean - 5, mean + 5])
+    #    bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
+    #    best_guess_mean = bin_centers[np.argmax(bin_heights)]
+    #    popt, _ = curve_fit(gaussian, bin_centers, bin_heights, p0=[mean, 0.3, 1.0], maxfev=10000)
+    #    mean = popt[0]
+    #    std = popt[-1]
+    x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
     #print(popt)
     if save_dir is not None:
         plt.plot(x_interval_for_fit, gaussian(x_interval_for_fit, *popt), color='k', label='fit: mu={:0.1f}, sig={:0.1f}'.format(mean, std))
-        plt.axvline(x=true_h0, linestyle='--', color='red', label='truth')
+        if true_h0 is not None:
+            plt.axvline(x=true_h0, linestyle='--', color='red', label='truth')
         plt.xlabel('H0 (km/Mpc/s)')
         plt.ylabel('density')
         plt.title('h0_histogram_{0:04d}'.format(lens_i))
