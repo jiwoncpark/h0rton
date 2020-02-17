32f6ca3ba11557dc331b077821a1d4f8a1ff5729
jiwoncpark <jiwon.christine.park@gmail.com>
2020-02-17 15:47:22 -0800

    Remove unused import

--- h0rton/train.py
+++ h0rton/train.py
@@ -18,7 +18,6 @@ import numpy as np # linear algebra
 from tqdm import tqdm
 # torch modules
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
