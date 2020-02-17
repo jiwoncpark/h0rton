bd52d1f154624bed1118ffbd0dbb94efe078cd4c
jiwoncpark <jiwon.christine.park@gmail.com>
2020-02-17 15:12:19 -0800

    Overlay PDF for non-scipy Gaussian approximation

--- h0rton/h0_inference/plotting_utils.py
+++ h0rton/h0_inference/plotting_utils.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 
 __all__ = ['plot_h0_histogram']
 
-def gaussian(x, mean, amplitude, standard_deviation):
+def gaussian(x, mean, standard_deviation, amplitude):
     return amplitude * np.exp( - ((x - mean) / standard_deviation) ** 2)
 
 def plot_h0_histogram(samples, weights, lens_i=0, true_h0=None, include_fit_gaussian=True, save_dir='.'):
@@ -13,20 +13,22 @@ def plot_h0_histogram(samples, weights, lens_i=0, true_h0=None, include_fit_gaus
 
     """
     bin_heights, bin_borders, _ = plt.hist(samples, weights=weights, bins=40, alpha=0.5, density=True, edgecolor='k', color='tab:blue', range=[50.0, 100.0])
+    bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
     if include_fit_gaussian:
         # Fit a gaussian
-        bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
         best_guess_mean = bin_centers[np.argmax(bin_heights)]
         popt, _ = curve_fit(gaussian, bin_centers, bin_heights, p0=[best_guess_mean, 0.3, 3.0], maxfev=10000)
         mean = popt[0]
-        std = popt[-1]
-        x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
-        # Overlay the fit gaussian pdf
-        plt.plot(x_interval_for_fit, gaussian(x_interval_for_fit, *popt), color='k', label='fit: mu={:0.1f}, sig={:0.1f}'.format(mean, std))
+        std = popt[1]
     else:
         # Compute the weighted mean and std analytically
         mean = np.average(samples, weights=weights)
         std = np.average((samples - mean)**2.0, weights=weights)**0.5
+        popt = [mean, std, 1.0/std/np.sqrt(2*np.pi)]
+    #x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
+    x_interval_for_fit = np.linspace(bin_centers[0], bin_centers[-1], 1000) 
+    # Overlay the fit gaussian pdf
+    plt.plot(x_interval_for_fit, gaussian(x_interval_for_fit, *popt), color='k', label='fit: mu={:0.1f}, sig={:0.1f}'.format(mean, std))
     #if std < 1.0:
     #    bin_heights, bin_borders, _ = plt.hist(samples, weights=weights, bins=80, alpha=0.5, density=True, edgecolor='k', color='tab:blue', range=[mean - 5, mean + 5])
     #    bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
@@ -40,7 +42,7 @@ def plot_h0_histogram(samples, weights, lens_i=0, true_h0=None, include_fit_gaus
             plt.axvline(x=true_h0, linestyle='--', color='red', label='truth')
         plt.xlabel('H0 (km/Mpc/s)')
         plt.ylabel('density')
-        plt.title('h0_histogram_{0:04d}'.format(lens_i))
+        plt.title('H0 posterior for lens {0:04d}'.format(lens_i))
         plt.legend()
         save_path = os.path.join(save_dir, 'h0_histogram_{0:04d}.png'.format(lens_i))
         plt.savefig(save_path)
