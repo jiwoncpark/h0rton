838db1162e8e30d670f3eb36e097ada1c36af590
jiwoncpark <jiwon.christine.park@gmail.com>
2020-04-12 21:33:43 -0700

    Add function for calculating sky brightness in magnitudes

--- h0rton/trainval_data/data_utils.py
+++ h0rton/trainval_data/data_utils.py
@@ -1,6 +1,32 @@
 import numpy as np
 import torch
-__all__ = ['rescale_01', 'stack_rgb', 'log_parameterize_Y_cols', 'whiten_Y_cols', 'plus_1_log', 'asinh']
+__all__ = ['rescale_01', 'stack_rgb', 'log_parameterize_Y_cols', 'whiten_Y_cols', 'plus_1_log', 'asinh', 'whiten_pixels', 'calculate_sky_brightness']
+
+def calculate_sky_brightness(flux_density=7.26*1e-19, lam_eff=15269.1):
+    """Calculate the sky brightness in mag with our zeropoint
+
+    Parameters
+    ----------
+    flux_density: float
+        the flux density in cgs units, defined per wavelength in angstroms (Default: 7.26*1e-19, estimated for 12500 ang and taken from Givialisco et al 2002)
+    lam_eff : float
+        the effective filter wavelength in angstroms (Default: for WFC3/IR F160W, 15279.1 ang). Taken from http://svo2.cab.inta-csic.es/svo/theory/fps3/index.php?id=HST/WFC3_IR.F160W
+
+    Note
+    ----
+    Zeropoint not necessary with absolute flux density values. Only when using cps.
+
+    """
+    # All spectral flux density units are per asec^2
+    # Zodiacal bg at the North Ecliptic Pole for WFC3, from Giavalisco et al 2002
+    flux_density_cgs_wave = flux_density #1.81*1e-18 # erg/cm^2/s^2/ang^1
+    # Convert spectral flux density for unit wavelength to that for unit frequency
+    flux_density_Jy = flux_density_cgs_wave*(3.34*1e4)*lam_eff**2.0 # Jy
+    mag_AB = -2.5*np.log10(flux_density_Jy) + 8.90
+    return mag_AB
+
+def whiten_pixels(pixels):
+    return (pixels - torch.mean(pixels))/torch.std(pixels)
 
 def asinh(x):
     return torch.log(x+(x**2+1)**0.5)
