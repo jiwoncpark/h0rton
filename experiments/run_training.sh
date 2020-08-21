3ef6209e8c1f99787cc84ccc77730017cb6a8839
jiwoncpark <jiwon.christine.park@gmail.com>
2020-08-21 15:23:06 -0700

    Modify order of training

--- experiments/run_training.sh
+++ experiments/run_training.sh
@@ -1,4 +1,4 @@
 python h0rton/train.py experiments/v1/train_val_cfg.json
 python h0rton/train.py experiments/v3/train_val_cfg.json
-#python h0rton/train.py experiments/v2/train_val_cfg.json
-#python h0rton/train.py experiments/v4/train_val_cfg.json
+python h0rton/train.py experiments/v2/train_val_cfg.json
+python h0rton/train.py experiments/v4/train_val_cfg.json
