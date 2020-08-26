79acd8b98610c1e7a24a1eae003eb4b01bc9f937
jiwoncpark <jiwon.christine.park@gmail.com>
2020-08-26 08:50:39 -0400

    Run remaining 0.5, 2 orbits

--- experiments/run_training.sh
+++ experiments/run_training.sh
@@ -2,7 +2,7 @@
 #python h0rton/train.py experiments/v3/train_val_cfg.json
 #python h0rton/train.py experiments/v2/train_val_cfg.json
 #python h0rton/train.py experiments/v4/train_val_cfg.json
-python h0rton/train.py experiments/v1/train_val_cfg_deeper.json
-python h0rton/train.py experiments/v3/train_val_cfg_deeper.json
-#python h0rton/train.py experiments/v2/train_val_cfg_deeper.json
-#python h0rton/train.py experiments/v4/train_val_cfg_deeper.json
+#python h0rton/train.py experiments/v1/train_val_cfg_deeper.json
+#python h0rton/train.py experiments/v3/train_val_cfg_deeper.json
+python h0rton/train.py experiments/v2/train_val_cfg_deeper.json
+python h0rton/train.py experiments/v4/train_val_cfg_deeper.json
