9349f866ad342fbb2cfe20a8c5daf6b5fbf7eefb
jiwoncpark <jiwon.christine.park@gmail.com>
2020-11-29 21:58:26 -0500

    Uncomment data generation bash script

--- experiments/generate_datasets.sh
+++ experiments/generate_datasets.sh
@@ -1,3 +1,3 @@
-#python -m baobab.generate baobab_configs/v7/test_v7_baobab_config.py
-#python -m baobab.generate baobab_configs/v7/val_v7_baobab_config.py
-#python -m baobab.generate baobab_configs/v7/train_v7_baobab_config.py
+python -m baobab.generate baobab_configs/v7/test_v7_baobab_config.py
+python -m baobab.generate baobab_configs/v7/val_v7_baobab_config.py
+python -m baobab.generate baobab_configs/v7/train_v7_baobab_config.py
